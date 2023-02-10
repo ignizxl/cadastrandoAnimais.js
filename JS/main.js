4601f03d719ed79e8b1373b5e566adfe28b23bd3
ignizxl <igor.joao@academico.ifpb.edu.br>
2023-02-10 03:02:10 -0300

    atualizando

--- JS/main.js
+++ JS/main.js
@@ -1,79 +1,82 @@
+//fazendo a importação dos arquivos dataset.js e do bovinos.js
+
 import dataset from "./model/dataset.js";
 import bovinos from "./model/bovinos.js";
 
+//definindo função pra calcular a idade dos animais em meses 
 function calcularIdadeEmMeses(datanasc) {
-	var anoAtual = new Date().getUTCFullYear()
-	var anoNasc = Number(datanasc.split('-')[0])
-	var idadeEmAno = anoAtual - anoNasc
-	var mesAtual = new Date().getUTCMonth()
-	var mesNasc = Number(datanasc.split('-')[1])
-	var idadeMes = (idadeEmAno * 12) - mesNasc + mesAtual
+  var anoAtual = new Date().getUTCFullYear();
+  var anoNasc = Number(datanasc.split("-")[0]);
+  var idadeEmAno = anoAtual - anoNasc;
+  var mesAtual = new Date().getUTCMonth();
+  var mesNasc = Number(datanasc.split("-")[1]);
+  var idadeMes = idadeEmAno * 12 - mesNasc + mesAtual;
 
-	return idadeMes
-	
+  return idadeMes;
 }
 
 function loadBovino() {
-	if (localStorage.getItem("bovinos-app:loaded") !== "ok") {
-		bovinos.load(dataset);
-		localStorage.setItem("bovinos-app:loaded", "ok");
-	}
-	for (const bovino of bovinos.readAll()) {
-		createBovinoView(bovino);
-	}
+  if (localStorage.getItem("bovinos-app:loaded") !== "ok") {
+    bovinos.load(dataset);
+    localStorage.setItem("bovinos-app:loaded", "ok");
+  }
+  for (const bovino of bovinos.readAll()) {
+    createBovinoView(bovino);
+  }
 }
 
+//formatação 
 function createBovinoView(bovino) {
-	const bovinosView = `
+  const bovinosView = `
  						<div class="col">
 			 				<div class="card">
 								<img src="${bovino.image}" alt="${bovino.raca}" class="card-img-top">
 								<div class="card-body">
 									<h5 class="card-Title">${bovino.raca}</h5>
-				 				<p class="card-text"> <b>Id: </b>${bovino.id}<br><b>Peso: </b>${bovino.peso} Kg<br><b>Data Nascimento: </b>${bovino.datanasc}<br>
+				 				<p class="card-text"> <b>Id: </b>${bovino.id}<br><b>Peso: </b>${
+    bovino.peso
+  } Kg<br><b>Data Nascimento: </b>${bovino.datanasc}<br>
 				 <b>Idade: </b>${calcularIdadeEmMeses(bovino.datanasc)} Meses
 							</div>
 						</div>
 			 		</div>
       `;
 
-	const bovinosDeck = document.querySelector(".card-deck");
+  const bovinosDeck = document.querySelector(".card-deck");
 
-	bovinosDeck.insertAdjacentHTML("beforeend", bovinosView);
+  bovinosDeck.insertAdjacentHTML("beforeend", bovinosView);
 }
 
 function loadFormValues(title, bovinoRaca, bovinoImage, bovinoDate) {
-	const formLabel = document.querySelector("#formBovinoLabel");
-	const bovinoRacaInput = document.querySelector("#raca-bovino");
-	const bovinoImageInput = document.querySelector("#bovino-imagem");
-	const bovinoDateInput = document.querySelector("#data-nasc")
+  const formLabel = document.querySelector("#formBovinoLabel");
+  const bovinoRacaInput = document.querySelector("#raca-bovino");
+  const bovinoImageInput = document.querySelector("#bovino-imagem");
+  const bovinoDateInput = document.querySelector("#data-nasc");
 
-	formLabel.innerHTML = title;
-	bovinoRacaInput.value = bovinoRaca;
-	bovinoImageInput.value = bovinoImage;
-	bovinoDateInput.value = bovinoDate;
-
+  formLabel.innerHTML = title;
+  bovinoRacaInput.value = bovinoRaca;
+  bovinoImageInput.value = bovinoImage;
+  bovinoDateInput.value = bovinoDate;
 }
 
 function loadFormCreateBovino() {
-	const formBovino = document.querySelector("#formBovino");
+  const formBovino = document.querySelector("#formBovino");
 
-	loadFormValues("Novo Bovino", "", "");
+  loadFormValues("Novo Animal", "", "");
 
-	formBovino.onsubmit = (e) => {
-		e.preventDefault();
+  formBovino.onsubmit = (e) => {
+    e.preventDefault();
 
-		let bovino = Object.fromEntries(new FormData(formBovino));
+    let bovino = Object.fromEntries(new FormData(formBovino));
 
-		const newBovino = bovinos.create(bovino);
+    const newBovino = bovinos.create(bovino);
 
-		createBovinoView(newBovino);
+    createBovinoView(newBovino);
 
-		document.querySelector("#newBtnBovino").blur();
-	};
+    document.querySelector("#newBtnBovino").blur();
+  };
 }
 
 window.loadFormCreateBovino = loadFormCreateBovino;
 
 loadBovino();
-
--- JS/model/dataset.js
+++ JS/model/dataset.js
@@ -1,11 +1,12 @@
+//vou deixar um animal de exemplo já cadastrado no dataset
 const dataset = [
-	{
-		id: Date.now(),
-		image: "./Img/angus-e-brangus.jpg",
-		raca: "Angus",
-		peso: 500,
-		datanasc: "2022-02-09",
-	},
+  {
+    id: Date.now(), //gerando o identificador aleatório
+    image: "./Img/highlandbull.jpg",
+    raca: "Senepol",
+    peso: 467,
+    datanasc: "2000-12-06",
+  },
 ];
 
 export default dataset;
